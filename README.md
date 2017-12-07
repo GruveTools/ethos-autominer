ad49b360cfc01b3c3e8562062aaa41b9c41e2005
Japh <japhie@gmail.com>
2017-12-08 04:37:46 +1100

    Split out config of configs and allow non-miner control run, add read me

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ethOS Autominer
 
 ## Usage
+
+_Backup your `local.conf` before running this script._
+
+The script file can live in the `/home/ethos` directory on your rig. Run the script using cron at your chosen interval, such as hourly with `0 * * * *`.
+
+You should also create a `scripts` and `configs` directory alongside the script. The `configs` directory should contain the various config files for the coins you want to mine, and you should copy the `config.sample.php` to `config.php` and edit it to refer to your config files and contain just the coins you want to switch between.
+
+If you pass `--dry-run` to the script, it won't do any of the miner restarting, _but will still switch configs_.
---
+++ config.sample.php
@@ -0,0 +1,27 @@
+<?php
+	$coins['BCN'] = array('hash_rate' => 2000.0, 'config' => 'minergate-bcn.conf'); // CryptoNight
+	$coins['HUSH'] = array('hash_rate' => 1720.0, 'config' => 'suprnova-hush.conf'); // Equihash
+	//$coins['HUSH'] = array('hash_rate' => 1720.0, 'config' => 'miningspeed-hush.conf'); // Equihash
+	$coins['ETH'] = array('hash_rate' => 104.0, 'config' => 'nanopool-eth.conf'); // Ethash
+	// $coins['MUSIC'] = array('hash_rate' => 104.0, 'config' => 'nomnom-music.conf'); // Ethash // Switch to exchange!! Too annoying syncing the wallet.
+	$coins['ORB'] = array('hash_rate' => 4200.0, 'config' => 'theblocksfactory-orb.conf'); // Neoscrypt
+	// $coins['SC'] = array('hash_rate' => 6400.0, 'config' => 'siamining-sia.conf'); // Blake (2b) // Switch to exchange!!
+	$coins['LBC'] = array('hash_rate' => 1080.0, 'config' => 'suprnova-lbc.conf'); // LBRY
+	//$coins['SIGT'] = array('hash_rate' => 106.0, 'config' => 'suprnova-sigt.conf'); // Skunk // Closed -> POW ended
+	$coins['ZEC'] = array('hash_rate' => 1720.0, 'config' => 'suprnova-zec.conf'); // Equihash
+	$coins['ZCL'] = array('hash_rate' => 1720.0, 'config' => 'suprnova-zcl.conf'); // Equihash
+	$coins['VTC'] = array('hash_rate' => 142000.0, 'config' => 'alwayshashing-vtc.conf'); // Equihash
+	$coins['XMR'] = array('hash_rate' => 2000.0, 'config' => 'nanopool-xmr.conf'); // CryptoNight
+	$coins['DGB'] = array('hash_rate' => 142.0, 'config' => 'suprnova-dgb.conf'); // Myriad-Groestl
+	$coins['DCR'] = array('hash_rate' => 10000.0, 'config' => 'suprnova-dcr.conf'); // Decred
+	$coins['PXC'] = array('hash_rate' => 4200.0, 'config' => 'theblocksfactory-pxc.conf'); // Neoscrypt
+	$coins['SIB'] = array('hash_rate' => 46.0, 'config' => 'suprnova-sib.conf'); // Neoscrypt
+	$coins['KMD'] = array('hash_rate' => 46.0, 'config' => 'suprnova-kmd.conf'); // Equihash
+	$coins['ZEN'] = array('hash_rate' => 46.0, 'config' => 'suprnova-zen.conf'); // Equihash
+	$coins['FTC'] = array('hash_rate' => 4200.0, 'config' => 'miningpoolhub-ftc.conf'); // Neoscrypt
+	$coins['UBIQ'] = array('hash_rate' => 104.0, 'config' => 'ubiqpool-ubiq.conf'); // Ethash
+	$coins['MONA'] = array('hash_rate' => 104.0, 'config' => 'suprnova-mona.conf'); // Lyra2REv2
+	//$coins['SOIL'] = array('hash_rate' => 104.0, 'config' => 'miners-zone-soil.conf'); // Ethash // Where's the wallet??
+	//$coins['SUMO'] = array('hash_rate' => 2000.0, 'config' => 'sumokoin-sumo.conf'); // CryptoNight // Switch to exchange!!
+	$coins['VTC'] = array('hash_rate' => 104.0, 'config' => 'miningpoolhub-vtc.conf'); // Lyra2REv2
+	//$coins['AEON'] = array('hash_rate' => 104.0, 'config' => 'sumominer-aeon.conf'); // Lyra2REv2 // Switch to exchange!!
